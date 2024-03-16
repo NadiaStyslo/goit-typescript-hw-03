a6760fddbb40ab0c358f9a11c50817e874f7a7d8
NadiaStyslo <sir.ihor@gmail.com>
2024-03-16 02:02:33 -0400

    finish

--- src/2.ts
+++ src/2.ts
@@ -17,7 +17,7 @@
 class Employee {
   // Заповніть модифікатори доступу
 
-  constructor(name: string, private department: string, protected salary: number) {}
+  constructor(public name: string, private department: string, protected salary: number) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,68 @@
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[];
+  constructor(door: boolean, key: Key) {
+    door: false;
+    key: Key;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  abstract OpenDoor(key: Key): void;
+}
+
+class myHouse extends House {
+  constructor(key: Key) {
+    super(true, key);
+  }
+  OpenDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door is opened.');
+    } else {
+      console.log(console.log('Door is closed.'));
+    }
+  }
+}
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new myHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
-
 export {};
